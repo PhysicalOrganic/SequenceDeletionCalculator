483b01fa56d72b95d6dfbb27c2bd219fd7c58221
James Howard <jamesrhoward6@gmail.com>
2024-10-21 23:13:22 -0600

    linted

--- README.md
+++ README.md
@@ -2,10 +2,7 @@
 This repository contains the code required to compute possible deletions of monomers from a sequence-defined oligourethane. The script is written in Python and has been tested with Python 3.11.0. No third party packages are required.
 
 ## Usage
-First, define your monomer library in the monomers.py file by including the 3-letter and one letter codes in the `THREE_LETTER_CODES` dictionary. Then specify the monomer 1-letter code and the monomer mass in the `ONE_LETTER_CODE_MASS_PAIRS` dictionary.
-<br>
-<br>
-Then specify the adducts you intend to see in your mass spectrum by defining them as `Adduct` objects in the adducts.py file.
+First, define your monomer library in the monomers.py file by including the 3-letter and one letter codes in the `THREE_LETTER_CODES` dictionary. Then specify the monomer 1-letter code and the monomer mass in the `ONE_LETTER_CODE_MASS_PAIRS` dictionary. Then specify the adducts you intend to see in your mass spectrum by defining them as `Adduct` objects in the adducts.py file.
 <br>
 <br>
 Run the Python script with the required arguments.
@@ -15,6 +12,5 @@ Run the Python script with the required arguments.
 Specify the sequence with either 1-letter codes (above) or using 3-letter codes (below)
 
 **Note**: 3-letter codes must be specified in quotation marks.
-
 
     python3 SequenceDeletionCalculator.py -i "Ala d2Tyr d2Tyr Ala"
--- monomers.py
+++ monomers.py
@@ -8,29 +8,29 @@
 Contains monomer definitions
 '''
 
-# monomer : mass pairs
+# monomer: mass pairs
 ONE_LETTER_CODE_MASS_PAIRS = {
-    'A': 101.04768, #A = Ala
-    'a': 103.06023, #a = d2Ala
-    'B': 115.06333, #B = Abu = Aminobutyric acid (Et side chain)
-    'b': 117.07588, #b = d2Abu
-    'V': 129.07898, #V = Val
-    'v': 131.09153, #v = d2Val
-    'S': 131.05824, #S = Ser (OMe)
-    's': 133.07080, #s = d2Ser (OMe)
-    'L': 143.09463, #L = Leu
-    'l': 145.10718, #l = d2Leu
-    'D': 145.03751, #D = Asp (unprotected COOH)
-    'K': 158.10553, #K = Lys
-    'E': 159.05316, #E = Glu (unprotected COOH)
-    'P': 177.07898, #P = Phe
-    'p': 179.09153, #p = d2Phe
-    'C': 183.12593, #C = Cha
-    'c': 185.13848, #c = d2Cha
-    'H': 191.09463, #H = HoPhe = Homophenylalaninol
-    'h': 193.10718, #h = d2HoPhe
-    'Y': 193.07389, #Y = Tyr (unprotected Phenol)
-    'y': 195.08645, #y = d2Tyr (unprotected Phenol)
+    'A': 101.04768,  # A = Ala
+    'a': 103.06023,  # a = d2Ala
+    'B': 115.06333,  # B = Abu = Aminobutyric acid (Et side chain)
+    'b': 117.07588,  # b = d2Abu
+    'V': 129.07898,  # V = Val
+    'v': 131.09153,  # v = d2Val
+    'S': 131.05824,  # S = Ser (OMe)
+    's': 133.07080,  # s = d2Ser (OMe)
+    'L': 143.09463,  # L = Leu
+    'l': 145.10718,  # l = d2Leu
+    'D': 145.03751,  # D = Asp (unprotected COOH)
+    'K': 158.10553,  # K = Lys
+    'E': 159.05316,  # E = Glu (unprotected COOH)
+    'P': 177.07898,  # P = Phe
+    'p': 179.09153,  # p = d2Phe
+    'C': 183.12593,  # C = Cha
+    'c': 185.13848,  # c = d2Cha
+    'H': 191.09463,  # H = HoPhe = Homophenylalaninol
+    'h': 193.10718,  # h = d2HoPhe
+    'Y': 193.07389,  # Y = Tyr (unprotected Phenol)
+    'y': 195.08645,  # y = d2Tyr (unprotected Phenol)
 }
 
 THREE_LETTER_CODES = {
@@ -40,19 +40,19 @@ THREE_LETTER_CODES = {
     'd2Abu': 'b',
     'Val': 'V',
     'd2Val': 'v',
-    'Ser' : 'S',
-    'd2Ser' : 's',
+    'Ser': 'S',
+    'd2Ser': 's',
     'Leu': 'L',
     'd2Leu': 'l',
     'Asp': 'D',
-    'Lys' : 'K',
+    'Lys': 'K',
     'Glu': 'E',
     'Phe': 'P',
-    'd2Phe' : 'p',
+    'd2Phe': 'p',
     'Cha': 'C',
-    'd2Cha' : 'c',
+    'd2Cha': 'c',
     'HoPhe': 'H',
-    'd2HoPhe' : 'h',
-    'Tyr' : 'Y',
-    'd2Tyr' : 'y'
+    'd2HoPhe': 'h',
+    'Tyr': 'Y',
+    'd2Tyr': 'y'
 }
